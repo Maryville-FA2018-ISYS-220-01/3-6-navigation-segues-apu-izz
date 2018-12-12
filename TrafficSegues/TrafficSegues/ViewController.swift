23ae709949c8c698534f0a09013dde19a3a56d26
apu-izz <31754074+apu-izz@users.noreply.github.com>
2018-12-11 20:21:35 -0600

    change

--- TrafficSegues/TrafficSegues/ViewController.swift
+++ TrafficSegues/TrafficSegues/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var UserNameText: UITextField!
+    
+    @IBOutlet weak var PassWordText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
+    }
+    @IBAction func LogIn(_ sender: UIButton) {
+    }
+    @IBAction func ForgetUserName(_ sender: UIButton) {
+    }
+    @IBAction func ForgetPassWord(_ sender: UIButton) {
+    }
+    guard let
+    sender = sender as? UIButton else
+    {
+            return
+    
+    }
 
+    if sender == forgotPasswordButton {
+    segue.destination.navigationItem.title = "Forgot Password"
+    } else if sender == forgotUsernameButton {
+    
+    segue.destination.navigationItem.title = "Forgot Username"
+    } else {
+    segue.destination.navigationItem.title =
+    usernameTextField.text
+    }
 }
 
